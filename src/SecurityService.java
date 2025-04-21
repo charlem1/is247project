eecfdfa816c9b49cd595f6026184b98e0af2efcc
Ridha235 <itshridayaa@gmail.com>
2025-04-21 09:26:16 -0400

    Update SecurityService.java

--- src/SecurityService.java
+++ src/SecurityService.java
@@ -1,7 +1,23 @@
 package com.club.service;
-
 import java.util.Date;
 import java.util.Random;
 
 public class SecurityService {
+   public boolean verifyAge(int age) {
+        return age >= 21; // Minimum legal drinking age
+    }
+
+    public boolean isVIP(Member member) {
+        return member.isVip() && verifyAge(member.getAge());
+    }
+
+    public String getAccessLevel(Member member) {
+        if (isVIP(member)) {
+            return "VIP Access Granted";
+        } else if (member.isPreRegistered()) {
+            return "Standard Access Granted";
+        } else {
+            return "Paper Wristband Issued - Limited Access";
+        }
+    }
 }

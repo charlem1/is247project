344d54b807e8a5b829175df9e5b7f1424b8b6019
Ridha <itshridayaa@gmail.com>
2025-04-23 21:52:18 -0400

    Update SecurityService.java

--- src/SecurityService.java
+++ src/SecurityService.java
@@ -2,21 +2,15 @@ package com.club.service;
 import java.util.Date;
 import java.util.Random;
 public class SecurityService {
-   public boolean verifyAge(int age) {
-        return age >= 21; // Minimum legal drinking age
-    }
-
-    public boolean isVIP(Member member) {
-        return member.isVip() && verifyAge(member.getAge());
-    }
-
-    public String getAccessLevel(Member member) {
-        if (isVIP(member)) {
-            return "VIP Access Granted";
-        } else if (member.isPreRegistered()) {
-            return "Standard Access Granted";
-        } else {
-            return "Paper Wristband Issued - Limited Access";
-        }
-    }
-}
+   public void validateAge(Member m) throws AgeRestrictionException{
+      if(m.age<21){
+         throw new AgeRestrictionException(" Must be 21+.");
+      }
+      System.out.println("Age validated.");
+   }
+   public Date getCurrentDate(){
+      return new Data();
+      public boolen randomCheck(){
+         return new Random().nextInt (bound:10)>7;
+      }
+   }

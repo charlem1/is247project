5dd774988988f91f734a29cb95e5003a1142c1f3
Ridha <itshridayaa@gmail.com>
2025-04-23 21:54:28 -0400

    Update SecurityService.java

--- src/SecurityService.java
+++ src/SecurityService.java
@@ -1,16 +1,22 @@
 package com.club.service;
 import java.util.Date;
 import java.util.Random;
+// Service class to handle security-related checks like age validation
 public class SecurityService {
-   public void validateAge(Member m) throws AgeRestrictionException{
-      if(m.age<21){
-         throw new AgeRestrictionException(" Must be 21+.");
+   // Validates if the member's age is 21 or above
+   public void validateAge(Member m) throws AgeRestrictionException {
+      if (m.age < 21) {
+         throw new AgeRestrictionException("Must be 21+.");
       }
       System.out.println("Age validated.");
    }
-   public Date getCurrentDate(){
-      return new Data();
-      public boolen randomCheck(){
-         return new Random().nextInt (bound:10)>7;
-      }
+
+   // Returns the current system date
+   public Date getCurrentDate() {
+      return new Date(); 
    }
+   // Performs a random check that returns true ~20% of the time
+   public boolean randomCheck() {
+      return new Random().nextInt(10) > 7; 
+   }
+}

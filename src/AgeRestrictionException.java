c1374f644f5bc314dc5b8be4b7254247323500b3
Myonam <myonam1@umbc.edu>
2025-05-04 12:45:45 -0400

    Update AgeRestrictionException.java

--- src/AgeRestrictionException.java
+++ src/AgeRestrictionException.java
@@ -1,6 +1,20 @@
 package com.club.service;
-
+/**
+ * Custom exception to indicate that a user has not met the minimum age requirement
+ * for a specific service or entry.
+ * <p>
+ * This exception is typically thrown when a person attempts to access a service
+ * or area restricted by age, such as a club requiring patrons to be 21+.
+ * </p>
+ * 
+ * @author 
+ */
 public class AgeRestrictionException extends Exception {
+    /**
+     * Constructs a new AgeRestrictionException with the specified detail message.
+     *
+     * @param msg the detail message explaining the reason for the exception
+     */
     public AgeRestrictionException(String msg) {
         super(msg);
     }

7736f0fc8299794abd09a10930ed20ba0d18972e
charlem1 <charlem1@umbc.edu>
2025-05-04 18:20:23 -0400

    Update Person.java
    added criteria

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,40 @@
 package com.club.service;
 
 public abstract class Person {
-  protected String name;
-  protected int age;
+    // Encapsulated fields
+    private String name;
+    private int age;
 
-  public Person(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-  public abstract void displayInfo();
+    // Abstract method to enforce subclass implementation
+    public abstract void displayInfo();
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Sets name
+    public void setName(String name) {
+        if (Utils.isValidName(name)) {
+            this.name = name;
+        }
+    }
+
+    // Gets ages
+    public int getAge() {
+        return age;
+    }
+
+    // Sets ages
+    public void setAge(int age) {
+        if (Utils.isValidAge(age)) {
+            this.age = age;
+        }
+    }
 }
